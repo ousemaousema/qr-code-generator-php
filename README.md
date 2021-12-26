89c1b6612c1446694e4a8ebfaecc8573b4111fef
ousemaousema <52883685+ousemaousema@users.noreply.github.com>
2021-12-26 17:27:57 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# programming-game
+
+# Programming Game
+
 This is a programming game only for programmers. And in this game you gonna complete some programming challenges. This game create in PHP Ajax. From Pure Coding.
+
+
+Challenge Page:-
+
+
+## Installation 
+
+For install this PHP game you need to download this one and then you will be move it to your htdocs folder and upload database that's it.
+
+
---
+++ challenge.php
@@ -0,0 +1,13 @@
+<?php include "vendor/header.php"; ?>
+<section class="homepage challenge-page">
+    <div class="container">
+        <div id="get_challenge">
+            <div id="loading-mask">
+                <div class="preloader">
+                    <div class="c-three-dots-loader"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include "vendor/footer.php"; ?>
